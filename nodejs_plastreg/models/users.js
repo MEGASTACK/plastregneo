82698567ae1f3bcea9580458710de47f8eae256b
John S <john.slev@gmail.com>
2014-05-19 17:40:02 -0400

    Two user types: Admin (can access keystone panel) and PlastRegUser (should be able to access plastreg specific stuff)

--- nodejs_plastreg/models/users.js
+++ nodejs_plastreg/models/users.js
@@ -14,12 +14,18 @@ User.add({
 	email: { type: Types.Email, initial: true, required: true, index: true },
 	password: { type: Types.Password, initial: true, required: false }
 }, 'Permissions', {
-	isAdmin: { type: Boolean, label: 'Can access Keystone' }
+	isAdmin: { type: Boolean, label: 'Can access Keystone' },
+	isPlastRegUser: { type: Boolean, label: "Regular PlastReg User"}
 });
 
 // Provide access to Keystone
 User.schema.virtual('canAccessKeystone').get(function() {
 	return this.isAdmin;
+});
+
+//Provide access to PlastReg stuff
+User.schema.virtual('canAccessPlastReg').get(function() {
+	return this.isPlastRegUser;
 });
 
 
